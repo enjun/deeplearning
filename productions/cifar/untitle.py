59006215f9dc29d6fe66c93afd18d2134f1953f1
enjun <936580775@qq.com>
2018-04-29 16:38:22 +0800

    update

--- productions/cifar/untitle.py
+++ productions/cifar/untitle.py
@@ -1,1 +1,13 @@
+"""
+This program can classify ten different things in dataset of cifar10
+"""
+import numpy as np
+import tensorboard as tf
+from keras.models import Sequential, Model
+from keras.layers.core import *
+import os
+import
+
+
+
 
